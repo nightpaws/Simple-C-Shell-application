9ca0b969a959fb942577c6927248ec19a9b3650b
nightpaws <nightpaws@hotmail.co.uk>
2014-02-17 12:47:24 +0000

    Fixed Tokenizer issues
    - Set array size up to 512 from 256
    - changed strncpy to array size -1 instead of +1
    - added descriptions to outputs for clarity
    - restored input handler
    - re-added version information
    - reversed logic of input handler
    - Reticulated Splines

--- ace4.c
+++ ace4.c
@@ -1,6 +1,3 @@
-/*
-  main.c
-  ACE4
 /* Version History =====================================================================
  * V0.1  07/02/2014 This is the first version of our program. Currently, we can take an
  *                  input, we have an indicator icon “>” and can exit the program with
@@ -17,9 +14,6 @@
  *                  then the input handler function begins termination.
  * ====================================================================================*/
 
-  Created by Me, Myself and I on 03/02/2014.
-  Copyright (c) 2014 Me, Myself and I. All rights reserved.
-*/
 /*
  main.c
  ACE4 v0.2
@@ -31,56 +25,69 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include "ace4.h"
-void command_selecter(char input){
+#include <unistd.h>
 
-return;
+#include "ace4.h"
+
+
+void command_selecter(char input){
+    
+    return;
 }
 
 void tokenizer(char* input){
-    char array[256];
+    char array[512];
 	strcpy(array, input);
-	strncpy(array, input, (sizeof(array) +1));
+	strncpy(array, input, (sizeof(array) -1)); //doesn't like this
     char *token = array;
     char *pointr = input;
-
+    
     while ((token = strtok(pointr, " ")) != NULL)
     {
-        printf("%s\n", token);
+        printf("token: %s\n", token);
         pointr = NULL;
     }
 	return;
 }
 
 bool input_handler() {
-    bool response = false;
-    char input[inputval];
-    printf(">");
-    fgets(input, in_size, stdin);
-	tokenizer(input);
-	printf("User Input: %s\n",input);
-	if(strcmp("exit\n",input)){
-		response = true;
-		printf("\n response is printing strange here for some reason %d",response);
-	}
+    bool terminate = false;
     
-    return response;
+    char input[inputval];
+    
+    while(terminate==false){
+        printf(">");
+        if(fgets(input, in_size, stdin)!=NULL){
+            printf("User untokenised input is: %s\n",input);
+            
+            /*If user enters "exit" then jump out */
+            if(strcmp("exit\n",input)==false){
+                break;
+            }
+            else
+            tokenizer(input);
+        }
+        else
+        /*Jump out of while loop*/
+            break;
+    }
+    terminate = true;
+    return terminate;
 }
+
+
 
 
 
 int main(int argc, char *argv[])
 {
-    printf("Simple Shell\n");
     printf("Simple Shell v0.2\n");
     printf("Created by CS210 Group 6 on 03/02/2014\n");
     printf("Copyright (c) 2014 CS210 Group 6, Strathclyde University. All rights reserved.\n\n");
     
     
-    while (input_handler()==true){}
+    while (input_handler()==false){}
     
-        
-    printf("\n\nTerminated Execution.\n\n");
     
     printf("\n\nTerminated Execution.\n\n");
     return 0;
