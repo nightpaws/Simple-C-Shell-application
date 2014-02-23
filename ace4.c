d7f37c42eacec108568fd5923cc76f1dd8371636
Epailes <stewartkey@hotmail.co.uk>
2014-02-23 18:47:24 +0000

    Tokenizer now stores tokens in an array, using another variable to keep track of how many items are added to the array we can keep track of how many tokens their are (this is necessary due to the way c stores uninitialized array positions in this type). There is a loop in command selector showing it printing through and keeping track of the location its at. Individual command selection currently does not work and has been added but is currently commented out.

--- ace4.c
+++ ace4.c
@@ -1,31 +1,30 @@
 /* Version History =====================================================================
- * V0.1  07/02/2014 This is the first version of our program. Currently, we can take an
- *                  input, we have an indicator icon “>” and can exit the program with
- *                  “exit”.
- *                  Ctrl+d exit remains to be implemented, as does strtok() tokenising, and
- *                  input validation.
- *
- * v0.2 13/02/2014 The structure of the input handler function has been altered to now
- *                  use a while loop. Copyright has been changed from default values from
- *                  Xcode to reflecting the group name. Initially this while loop used
- *                  multiple if>else if statements, however it now descends with break
- *                  lines when an exit command is triggered. The boolean value for response
- *                  has been renamed terminate to improve readability. If terminate is true,
- *                  then the input handler function begins termination.
- *
- * v0.3 19/02/2014 This version has a functional tokenizer and accounts for all the required
- *                  token types. It can handle up to 512 characters however it behaves in
- *                  a strange way after 512 characters and outputs a second \n output if the
- *                  512 boundary is exceeded.
- * ====================================================================================*/
+* V0.1 07/02/2014 This is the first version of our program. Currently, we can take an
+* input, we have an indicator icon “>” and can exit the program with
+* “exit”.
+* Ctrl+d exit remains to be implemented, as does strtok() tokenising, and
+* input validation.
+*
+* v0.2 13/02/2014 The structure of the input handler function has been altered to now
+* use a while loop. Copyright has been changed from default values from
+* Xcode to reflecting the group name. Initially this while loop used
+* multiple if>else if statements, however it now descends with break
+* lines when an exit command is triggered. The boolean value for response
+* has been renamed terminate to improve readability. If terminate is true,
+* then the input handler function begins termination.
+*
+* v0.3 19/02/2014 This version has a functional tokenizer and accounts for all the required
+* token types. It can handle up to 512 characters however it behaves in
+* a strange way after 512 characters and outputs a second \n output if the
+* 512 boundary is exceeded.
+* ====================================================================================*/
 
 /*
- main.c
- ACE4 v0.3
- 
- Created by Group 6 on 19/02/2014.
- Copyright (c) 2014 Group 6. All rights reserved.
- */
+main.c
+ACE4 v0.3
+Created by Group 6 on 19/02/2014.
+Copyright (c) 2014 Group 6. All rights reserved.
+*/
 
 #include <stdio.h>
 #include <string.h>
@@ -33,27 +32,123 @@
 #include <unistd.h>
 
 #include "ace4.h"
-
-
-void command_selecter(char input){
-    
+char *array[max_args][max_chars];
+int ntokens;
+/*
+void cd(char tokens[max_args]){
+    printf("cd has been selected\n");
     return;
 }
 
-void tokenizer(char* input){
-    char array[512];
-    printf("\nLENGTH: %lu\n",strlen(input));
+void pwd(char tokens[max_args]){
+    printf("pwd has been selected\n");
+    return;
+}
 
-    
-         strtok(input,"\n");
-        strncpy(array, input, (sizeof(array) -1));
-        char *token = array;
-        char *pointr = input;
-    
-        while ((token = strtok(pointr, "<>| \t")) != NULL)
-        {
-            printf("token: '%s'\n", token);
-            pointr = NULL;
+void getpath(char tokens[max_args]){
+    printf("getpath has been selected\n");
+    return;
+}
+
+void setpath(char tokens[max_args]){
+    printf("setpath has been selected\n");
+    return;
+}
+
+void history(char tokens[max_args]){
+    printf("history has been selected\n");
+    return;
+}
+
+void runlast(char tokens[max_args]){
+    // TODO add in special case for !! commands
+    printf("runlast has been selected\n");
+    return;
+}
+
+void alias(char tokens[max_args]){
+    printf("alias has been selected\n");
+    return;
+}
+
+void printalias(){
+    printf("printalias has been selected\n");
+    return;
+}
+
+void unalias(char tokens[max_args]){
+    printf("unalias has been selected\n");
+    return;
+}
+*/
+
+
+
+
+void command_selecter(){
+   /* if(strcmp("cd",array[0])==true)
+    {
+        cd(array[1]);
+    }
+    else if(strcmp("pwd",array[0])==true)
+    {
+        pwd(array[1]);
+    }
+    else if(strcmp("getpath",array[0])==true)
+    {
+        getpath(array[0]);
+    }
+    else if(strcmp("setpath",array[0])==true)
+    {
+        setpath(array[0]);
+    }
+    else if(strcmp("history",array[0])==true)
+    {
+        history(array[0]);
+    }
+    else if(strcmp("!",array[0])==true)
+    {
+        runlast(array[0]);
+    }
+    else if(strcmp("alias",array[0])==true)
+    {
+        if(array[0] != NULL){
+            alias(*array);
+        } else {
+            printalias();
+        }
+    }
+    else if(strcmp("unalias",array[0])==true)
+    {
+        unalias(array[0]);
+    }
+    else {
+         Just run the command and catch the error 
+        
+    }
+*/
+
+
+    int i = 0;
+    while(i<ntokens){
+    printf("Token: '%s', counter is %d\n",array[i], i);
+    i = i+1;
+	}
+    return;
+}
+
+void tokenizer(char *input){
+    printf("\nLENGTH: %lu\n",strlen(input));
+    strtok(input,"\n");
+    char *token;
+    char *pointr = input;
+    int i = 0;
+    while ((token = strtok(pointr, "<>| \t")) != NULL)
+    {
+	strcpy(array[i], token);
+        pointr = NULL;
+	ntokens = ntokens + 1;
+	i = i + 1;
         }
     return;
 }
@@ -76,8 +171,9 @@ bool input_handler() {
             }
             else{
                 /**remove the trailing newline*/
+	    ntokens = 0;
             tokenizer(input);
-                
+            command_selecter();
             }
             }
     else
@@ -105,40 +201,39 @@ int main(int argc, char *argv[])
     
     
     
-//    char *get_line (char *string, size_t n, FILE *f);
-//    {
-//        char *p = fgets (string, n, f); //p = fgets (line, BUFFER_LENGTH, stdin);
-//        
-//        if (p != NULL) {
-//            size_t last = strlen(string) - 1;
-//            
-//            if (string[last] == '\n') string[last] = '\0';
-//        }
-//        return p;
-//    }
-//    Find user home directory from the environment
-//    Set current working directory to user home directory
-//    Keep original path
-//    Load
-//    aliases
-//    Load history
-//    Do while shell has not terminated
-//        Display prompt
-//        Read and parse user input
-//        If input is a history invocation then retrieve command from history
-//        Else add command to history
-//        If input is an alias then replace the alias with the origin
-//        al command
-//        If command is built
-//        -
-//        in invoke appropriate function
-//        Else execute command as an external process
-//        End while
-//            Save history
-//            Save aliases
-//            Restore original path
-//            Exit
+// char *get_line (char *string, size_t n, FILE *f);
+// {
+// char *p = fgets (string, n, f); //p = fgets (line, BUFFER_LENGTH, stdin);
+//
+// if (p != NULL) {
+// size_t last = strlen(string) - 1;
+//
+// if (string[last] == '\n') string[last] = '\0';
+// }
+// return p;
+// }
+// Find user home directory from the environment
+// Set current working directory to user home directory
+// Keep original path
+// Load
+// aliases
+// Load history
+// Do while shell has not terminated
+// Display prompt
+// Read and parse user input
+// If input is a history invocation then retrieve command from history
+// Else add command to history
+// If input is an alias then replace the alias with the origin
+// al command
+// If command is built
+// -
+// in invoke appropriate function
+// Else execute command as an external process
+// End while
+// Save history
+// Save aliases
+// Restore original path
+// Exit
     
 
 }
-
--- ace4.h
+++ ace4.h
@@ -7,3 +7,8 @@ typedef int bool;
 #define inputval 512
 /*declare max size of input plus one for null value*/
 #define in_size inputval+1
+
+/* Max size of arg array */
+#define max_args 50
+/* Define number of chars per arg in array */
+#define max_chars 513
